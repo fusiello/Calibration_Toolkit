3653b517907e4eee97cbd2decb715723dd56960b
Andrea <andrea.fusiello@uniud.it>
2024-05-22 15:03:26 +0200

    minor fixes

--- templateMatching/tempMatching.m
+++ templateMatching/tempMatching.m
@@ -41,19 +41,21 @@ function cumCorrScore = tempMatching(frameImg, templateImg, method)
     stdFrame = sqrt(conv2(frameGray.^2,ones(size(templateGray))./numel(templateGray),'same')-frameMean.^2);
     corrPartII = frameMean.*sum(templateGray(:)-templateMean);
     
-    corrPartI = conv2(frameGray,templateGray-templateMean,'same')./numel(templateGray);
+    corrPartI = conv2(frameGray,rot90(templateGray-templateMean,2),'same')./numel(templateGray);
     cumCorrScore =  (corrPartI-corrPartII)./(stdFrame.*stdTemplate);
     
     % rotate the same template 3 times by 90 deg
     if strcmp(method, 'rot')
         
+        
+        corrPartI = conv2(frameGray,templateGray-templateMean,'same')./numel(templateGray);
+        corrScore =  (corrPartI-corrPartII)./(stdFrame.*stdTemplate);
+        cumCorrScore =  max(cumCorrScore,corrScore);
+        
         corrPartI = conv2(frameGray,rot90(templateGray-templateMean,1),'same')./numel(templateGray);
         corrScore =  (corrPartI-corrPartII)./(stdFrame.*stdTemplate);
         cumCorrScore =  max(cumCorrScore,corrScore);
         
-        corrPartI = conv2(frameGray,rot90(templateGray-templateMean,2),'same')./numel(templateGray);
-        corrScore =  (corrPartI-corrPartII)./(stdFrame.*stdTemplate);
-        cumCorrScore =  max(cumCorrScore,corrScore);
         
         corrPartI = conv2(frameGray,rot90(templateGray-templateMean,3),'same')./numel(templateGray);
         corrScore =  (corrPartI-corrPartII)./(stdFrame.*stdTemplate);
